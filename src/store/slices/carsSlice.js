2f5a0b3e7a5dfb13aba7a3118c31ab54e9378b0f
Glenn Pinto <gvpinto@gmail.com>
2023-07-22 07:38:35 -0400

    Section 20 Complete

---
+++ src/store/slices/carsSlice.js
@@ -0,0 +1,39 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+
+const carsSlice = createSlice({
+    name: 'cars',
+    initialState: {
+        searchTerm: '',
+        data: []
+    },
+    reducers: {
+        changeSearchTerm(state, action) {
+            state.searchTerm = action.payload;
+        },
+        addCar(state, action) {
+            const { name, cost } = action.payload;
+            state.data.push({
+                id: nanoid(),
+                name,
+                cost,
+            });
+        },
+        removeCar(state, action) {
+            const updated = state.data.filter((car) => {
+                return car.id !== action.payload;
+            });
+
+            state.data = updated;
+        }
+    }
+});
+
+
+export const {
+    changeSearchTerm,
+    addCar,
+    removeCar
+} = carsSlice.actions;
+
+export const carsReducer = carsSlice.reducer;
